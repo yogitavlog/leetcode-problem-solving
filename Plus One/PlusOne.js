a3c5ab95ce164d80114e4bb49471c64b271a570f
Yogita Varshney <67826345+yogitavlog@users.noreply.github.com>
2021-12-18 14:43:21 +0530

    Add files via upload

---
+++ Plus One/PlusOne.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let carry = 1;
+    for(let i=digits.length-1; i>=0; i--){
+        digits[i] = digits[i]+carry;
+        carry = Math.floor(digits[i]/10);
+        digits[i] = digits[i]%10;
+    }
+    
+    if(carry > 0){
+        digits.unshift(carry);
+    }
+    
+    return digits;
+};
